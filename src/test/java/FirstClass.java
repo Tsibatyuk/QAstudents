fee3d44d0e51080d2a8737a5a02132e0a5938389
Tsibatyuk <zhtsibatyuk@gmail.com>
2023-07-23 16:50:28 +0300

    test

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -1,6 +1,19 @@
+import org.testng.annotations.Test;
+
 public class FirstClass {
 
     public static void main(String[] args) {
         System.out.println("Hello, Word");
     }
+String method = "12";
+
+     void someMethod1(){
+
+    }
+
+    @Test
+    void someMethod(){
+        System.out.println();
+    }
+
 }
