3a21280ebb690e58e1faf85fbb0d4356e0f61dd0
Your Name <you@example.com>
2023-07-26 18:34:12 +0300

    Hi

---
+++ src/test/java/pisun/Pinteres.java
@@ -0,0 +1,9 @@
+import abstractpages.BaseTest;
+import org.testng.annotations.Test;
+
+public class Pinteres extends BaseTest {
+@Test
+ public void soNe2(){openUrl(openUrl("https://hoff.ru/iwaf-captcha"););}
+
+public void closerText(){}
+}
