c53f088a168e2c4fe7987fe6ccb2feb4d3553243
Your Name <you@example.com>
2023-07-27 13:00:05 +0300

    вибиває помилку ,не можу виправити

---
+++ src/test/java/pisun/Katana.java
@@ -0,0 +1,14 @@
+package pisun;
+
+
+import org.testng.annotations.Test;
+
+@Test
+
+public class Katana {
+            public static void main(String[] args) {
+                for (int i = 2; i < 10; i++) {
+                    System.out.println(i);
+                }
+            }
+        }
