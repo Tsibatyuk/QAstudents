75249e3f405d1ed21071c261fb6d2711c2f1762e
Tsibatyuk <zhtsibatyuk@gmail.com>
2023-07-27 13:38:37 +0300

    conflict

--- src/test/java/pisun/Pinteres.java
+++ src/test/java/pisun/Pinteres.java
@@ -1,9 +1,10 @@
+package pisun;
+
 import abstractpages.BaseTest;
 import org.testng.annotations.Test;
 
 public class Pinteres extends BaseTest {
 @Test
- public void soNe2(){openUrl(openUrl("https://hoff.ru/iwaf-captcha"););}
-
-public void closerText(){}
+ public void soNe2(){openUrl("https://hoff.ru/iwaf-captcha");
+}
 }
