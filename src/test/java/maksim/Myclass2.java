8c80f8e801ea06377a55d3e316390b3b42285af5
Tsibulko <fisher481516@gmail.com>
2023-07-27 13:30:54 +0300

    add a cicle

---
+++ src/test/java/maksim/Myclass2.java
@@ -0,0 +1,16 @@
+package maksim;
+
+import org.testng.annotations.Test;
+
+public class Myclass2{
+
+double X = 2.2;
+
+    @Test
+    public void cv() {
+
+        for (double X = 2.2;X <=10;X++ )
+
+            System.out.println(X);
+    }
+}
