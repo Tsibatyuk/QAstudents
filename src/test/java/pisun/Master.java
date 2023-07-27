2467e67349dd0924a78049752ac16cc52045b720
Your Name <you@example.com>
2023-07-27 12:04:38 +0300

    тренуюсь з циклами

--- src/test/java/pisun/Master.java
+++ src/test/java/pisun/Master.java
@@ -1,2 +1,12 @@
-package pisun;public class Master {
+package pisun;
+
+import org.testng.annotations.Test;
+
+public class Master {
+    @Test
+public void Master(){
+    for (int i = 0; i < 10; i++) {
+        System.out.println(x);
+    }
+}
 }
