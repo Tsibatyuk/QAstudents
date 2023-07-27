152eafe71fd3cf3f9f01f569cffca14bf54c99b1
Tsibulko <fisher481516@gmail.com>
2023-07-27 13:37:29 +0300

    update cicle

--- src/test/java/maksim/Myclass2.java
+++ src/test/java/maksim/Myclass2.java
@@ -4,12 +4,12 @@ import org.testng.annotations.Test;
 
 public class Myclass2{
 
-double X = 2.2;
+double X = 2.5;
 
     @Test
     public void cv() {
 
-        for (double X = 2.2;X <=10;X++ )
+        for (double X = 2.5;X <=10;X++ )
 
             System.out.println(X);
     }
