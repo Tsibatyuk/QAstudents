cfd89f34654b0b3bbf154a89def49ee0988287e0
Tsibulko <fisher481516@gmail.com>
2023-07-27 15:24:20 +0300

    add a condition

--- src/test/java/maksim/Myclass2.java
+++ src/test/java/maksim/Myclass2.java
@@ -2,15 +2,33 @@ package maksim;
 
 import org.testng.annotations.Test;
 
-public class Myclass2{
+public class Myclass2 {
 
-double X = 2.5;
+    double X = 2.5;
 
     @Test
     public void cv() {
 
-        for (double X = 2.5;X <=10;X++ )
+        for (double X = 2.5; X <= 10; X++) {
+            System.out.print(X);
+        }
 
-            System.out.println(X);
     }
+    int x = 50;
+    int y = 100;
+        @Test
+        public void nb() {
+
+            if(x>y){
+                System.out.println("x>y");
+            }else {
+                System.out.println("y>x");
+            }
+
+
+        }
 }
+
+
+
+
