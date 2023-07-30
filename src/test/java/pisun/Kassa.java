5ad53732f9fbc7bb6af1a45437467ac742809dd1
Tsibatyuk <zhtsibatyuk@gmail.com>
2023-07-30 12:41:26 +0300

    check the errors

--- src/test/java/pisun/Kassa.java
+++ src/test/java/pisun/Kassa.java
@@ -1,11 +1,22 @@
 package pisun;
 
-public class Kassa {
+import org.testng.annotations.Test;
 
-    void Sisa( {
-        for (int l = 1; l < 70; l++){
-            System.out.print(l);
-            if (l=1);l<
+public class Kassa {
+@Test
+    void Sisa() {
+        for (int l = 0; l < 70; l++){
+
+            if (l==1){
+                System.out.println("l дорівнює 1");
+                l=3;
+            }if (l==2){
+                System.out.println("l дорівнює 2");
+            }if (l==3){
+                System.out.println("l дорівнює 3");
+            }else {
+                System.out.print("1");
+            }
         }
     }
 }
