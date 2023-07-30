fbe509cd01aa60c875c0f1898795be4fdd33edc7
Your Name <you@example.com>
2023-07-30 11:47:14 +0300

    Son2

--- src/test/java/pisun/Katana.java
+++ src/test/java/pisun/Katana.java
@@ -5,11 +5,46 @@ import org.testng.annotations.Test;
 
 
 
-public class Katana{
+public class Katana {
     @Test
-            public void User() {
-                for (int i = 2; i < 10; i++) {
-                    System.out.println(i);
-                }
+    public void User() {
+        for (int k = -1; k < 30; k++) {
+            System.out.println(k);
+
+
+
+
+            for (int k = 5; a < 25; a++) {
+                System.out.print(a);
+
+
+
+
+                for (int i = 7; i < 20; i++) {
+                    System.out.print(i);
+
+
+
+
+                    for (int c = 10; k < 15; c++) {
+                        System.out.print(c);
+
+
+
+                        for (int n = 15; k < 15; n++) {
+                            System.out.print(n);
+                        }
+
+                }}}}
             }
         }
+
+        public class Kassa{
+
+     void Sisa( {
+         for (int l = 1; l < 70; l++){
+             System.out.print(l);
+             if (l=1);l<
+         }
+     }
+}
