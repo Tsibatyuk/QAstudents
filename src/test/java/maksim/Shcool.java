153542dbc715ebf8c5b6f85162ba3681d25dfcac
Tsibulko <fisher481516@gmail.com>
2023-08-03 16:17:18 +0300

    add a linkedlist

--- src/test/java/maksim/Shcool.java
+++ src/test/java/maksim/Shcool.java
@@ -1,4 +1,39 @@
 package maksim;
 
-public class Shcool {
+import java.util.LinkedList;
+
+public class Shcool implements W,C,D{
+    @Override
+    public void hh(String[] args) {
+
+                LinkedList list = new LinkedList();
+                list.add(1);
+                list.add(2);
+                list.add(3);
+    }
+    @Override
+    public void gg() {
+        System.out.print(2);
+
+    }
+
+    @Override
+    public void jj() {
+
+    }
+
+    @Override
+    public void ff() {
+
+    }
+
+    @Override
+    public void aa() {
+
+    }
+
+    @Override
+    public void ss() {
+
+    }
 }
--- src/test/java/maksim/W.java
+++ src/test/java/maksim/W.java
@@ -1,6 +1,8 @@
 package maksim;
 
 public interface W {
+    void hh(String[] args);
+
     public void aa();
 
     public  void ss();
