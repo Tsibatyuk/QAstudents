3f74a62f07b30a7e00606fc49fa7408e1e0f3a81
OlenaMyla <njonja1309@gmail.com>
2023-08-08 14:08:23 +0300

    зміна числа

--- src/test/java/olena/klass2.java
+++ src/test/java/olena/klass2.java
@@ -1,2 +1,22 @@
-package olena;public class klass2 {
-}
+package olena;
+
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+public class klass2 {
+ @Test
+    public void qw() {
+        List<Integer> str = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+           str.add(i);}
+        List<Integer> evenNumbers = str.stream()
+                .filter(num -> num % 2 == 0)
+                .sorted()
+                .collect(Collectors.toList());
+
+        evenNumbers.forEach(System.out::println);
+        }
+    }
+
