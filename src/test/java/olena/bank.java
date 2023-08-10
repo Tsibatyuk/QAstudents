b97b74ed18ec18099c052501affede529f6f4dd9
OlenaMyla <njonja1309@gmail.com>
2023-08-10 12:04:29 +0300

    нова робота в scanner додали коректив з пробілами

--- src/test/java/olena/bank.java
+++ src/test/java/olena/bank.java
@@ -7,18 +7,18 @@ public class bank {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int kredit = scanner.nextInt();
-        System.out.println("сума"+ kredit +"вже отриманого кредиту");
+        System.out.println("сума "+ kredit +" вже отриманого кредиту");
         int morekredit = scanner.nextInt();
         if (morekredit > 0) {
-            System.out.println("сума" + morekredit + "як додатковий кредит");
+            System.out.println("сума " + morekredit + " як додатковий кредит ");
         } else {
-            System.out.println("кредитор не взяв грошей");}
+            System.out.println("кредитор не взяв грошей ");}
             int summ = kredit + morekredit;
-            System.out.println("повна сумма кредиту" + summ );
+            System.out.println("повна сумма кредиту " + summ );
             if (summ > 100000) {
                 int morelimit = summ - 100000;
                 int penaly = morelimit * 10;
-                System.out.println("кредит був переви на" + morelimit + "через що кретор має ваплатити штраф 10% від суми надлишку, яка складе"+ penaly);
+                System.out.println("кредит був переви на " + morelimit + " через що кретор має ваплатити штраф 10% від суми надлишку, яка складе "+ penaly);
             }
 
         }
