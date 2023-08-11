d8ebdeafedd3fc01469831b82c283f26654fff07
venya2201 <benjamin02226@gmail.com>
2023-08-11 12:57:03 +0300

    очманіти яке складне завдання

---
+++ src/test/java/venya/Class26.java
@@ -0,0 +1,32 @@
+package venya;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Class26 {
+    public static void main(String[] args) {
+        List<Character> qm = new ArrayList<>();
+
+
+
+        for (char b = 'a'; b <= 'z'; b++) {
+            qm.add(b);
+        }
+        Map<Integer, String> abc = new HashMap<>();
+        int counter = 1;
+        for (int i = 0; i <= qm.size(); i++) {
+            if ((i + 1) % 2 == 0) {
+                abc.put(counter, String.valueOf(qm.get(i)));
+                counter++;
+            }
+        }
+                for (Map.Entry<Integer, String> entry : abc.entrySet()) {
+                    System.out.println("Position: " + entry.getKey() + ", b: " + entry.getValue());
+                }
+            }
+
+        }
+
+
