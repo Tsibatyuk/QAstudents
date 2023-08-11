6349b814e5816d442db208533ac3d5989fc3af26
OlenaMyla <njonja1309@gmail.com>
2023-08-11 17:20:11 +0300

    калькулятор

---
+++ src/test/java/olena/Calculator.java
@@ -0,0 +1,18 @@
+package olena;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int q = scanner.nextInt();
+        String a = scanner.next();
+        int s = scanner.nextInt();
+        int b = q-s;
+
+        System.out.println(b);
+
+
+
+    }
+}
