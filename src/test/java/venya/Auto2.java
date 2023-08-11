821e9a9b45d28d8faa267f149abeb47f84f80984
venya2201 <benjamin02226@gmail.com>
2023-08-11 15:10:22 +0300

    щшкрпшщук

--- src/test/java/venya/Auto2.java
+++ src/test/java/venya/Auto2.java
@@ -1,2 +1,35 @@
-package venya;public class Auto2 {
+package venya;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Auto2 {
+
+    public static void main(String[] args) {
+        List<String> qm = new ArrayList<>();
+
+        qm.add("BMW");
+        qm.add("Mercedes");
+        qm.add("Toyota");
+        qm.add("Tesla");
+        qm.add("Volvo");
+        qm.add("Kia");
+        qm.add("Ford");
+        qm.add("Bugatti");
+        qm.add("Lamborghini");
+        qm.add("Chevrolet");
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("введіть букву");
+        String input = scanner.nextLine();
+
+        List<String> filteredCars = qm.stream()
+                .filter(car -> car.startsWith(input))
+                .toList();
+
+        System.out.println("cars starting with '" + input + "': " + filteredCars);
+    }
+
 }
