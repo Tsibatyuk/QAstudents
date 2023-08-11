3ea876dc15b0ab29d02e014fadf6828113fa52a8
Ilia <Kylymystyy@gmail.com>
2023-08-11 16:32:48 +0300

    Calculator

--- src/test/java/ilia/Class8.java
+++ src/test/java/ilia/Class8.java
@@ -6,9 +6,47 @@ public class Class8 {
 
     public static void main(String[] args)
     {
-        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
 
-        int rundomNumber = random.nextInt(100);
-        System.out.println("Випадкове число: " + rundomNumber);
+        System.out.print("Enter your first number = ");
+        double firstNumber = scanner.nextDouble();
+
+        System.out.print("Enter your operator( + , * , - , /) ");
+        char operator = scanner.next().charAt(0);
+
+        System.out.print("Enter your second number = ");
+        double secondNumber = scanner.nextDouble();
+
+        double result = 0.0;
+
+        switch (operator)
+        {
+            case '+':
+                result = firstNumber + secondNumber;
+            break;
+            case '-':
+                result = firstNumber - secondNumber;
+            break;
+            case '*':
+                result = firstNumber * secondNumber;
+            break;
+            case '/':
+                if (secondNumber != 0)
+                {
+                    result = firstNumber / secondNumber;
+                }
+                else
+                {
+                    System.err.println("Error, you can not divide for zero");
+                    return;
+                }
+            break;
+            default:
+                System.err.println("Wrong operator");
+            return;
+        }
+        System.out.println("Result = " + result);
+
+
     }
 }
