4a3b92239101520d3d80355e4f837643142a1499
Ilia <Kylymystyy@gmail.com>
2023-08-11 13:00:22 +0300

    I do it

---
+++ src/test/java/ilia/Class8.java
@@ -0,0 +1,13 @@
+package ilia;
+
+import java.util.*;
+
+public class Class8 {
+
+    public static void main(String[] args)
+    {
+        List<String> berries = new ArrayList<>();
+        berries.add("I am go to ass");
+        System.out.println(berries);
+    }
+}
