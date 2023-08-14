c73affbd6dbc1389a5436352a69410f56b29c64b
Tsibatyuk <zhtsibatyuk@gmail.com>
2023-08-14 08:23:11 +0300

    make changes in Maksim test

--- src/test/java/maksim/Person.java
+++ src/test/java/maksim/Person.java
@@ -6,43 +6,41 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Person {
-    private static Comparator<? super Person> getName;
     String gender;
     String name;
-    int age = 4;
+    int age;
 
-    public Person(String іван, int i, String м) {
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
     }
 
 
-    public void uu(){
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
-}
-public String getName(){
-   return name;}
-public int getAge(){
-    return age;
-}
-public String getGender(){
-    return gender;
-}
-
-    public  static void main(String[] args) {
-
-    List<Person> people = new  ArrayList<>();
+    public static void main(String[] args) {
+        List<Person> people = new ArrayList<>();
         people.add(new Person("Анна", 30, "Ж"));
         people.add(new Person("іван", 28, "М"));
         people.add(new Person("Марія", 22, "Ж"));
         people.add(new Person("Петька", 35, "М"));
         people.add(new Person("Екатерина", 26, "Ж"));
 
-
         List<String> filtered = people.stream()
                 .filter(person -> person.getAge() > 25)
-                .sorted(Person.getName)
-                .map(person -> person.getName() + " - " + person.getAge())
+                .sorted(Comparator.comparing(Person::getName))
+                .map(person -> person.getName() + " "+person.getGender()+" - " + person.getAge())
                 .collect(Collectors.toList());
 
         filtered.forEach(System.out::println);
