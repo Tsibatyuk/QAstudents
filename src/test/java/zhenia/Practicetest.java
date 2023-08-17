3fe9b745ec1c3461c66947464c2941ff7ba8c74e
Tsibatyuk <zhtsibatyuk@gmail.com>
2023-08-17 16:52:35 +0300

    contructor1

--- src/test/java/zhenia/Practicetest.java
+++ src/test/java/zhenia/Practicetest.java
@@ -3,6 +3,7 @@ package zhenia;
 import org.testng.annotations.Test;
 
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Practicetest {
@@ -58,5 +59,147 @@ public class Practicetest {
         }
 
 
+        @Test
+    public void streamLambdaMethod()
+    {
+        List<String> words = new ArrayList<>();
+        words.add("Book");
+        words.add("Sun");
+        words.add("Bicycle");
+        words.add("Ice Cream");
+        words.add("Ocean");
+        words.add("Coffee");
+        words.add("Wind");
+        words.add("Guitar");
+        words.add("Basket");
+        words.add("Medal");
+        words.add("Park");
+        words.add("Candle");
+        words.add("Store");
+        words.add("Koala");
+        words.add("Park");
+        words.add("Store");
+        words.add("Rain");
+        words.add("Lantern");
+        words.add("Milk");
+        words.add("Rocket");
+        words.add("Theatre");
+        words.add("Flamingo");
+        words.add("Chocolate");
+        words.add("Summer");
+        words.add("Garland");
+        words.add("Pizza");
+        words.add("Cow");
+        words.add("Pasta");
+        words.add("Water");
+        words.add("Computer");
+
+//        List<String> sortedAlphabet = words.stream()
+//                .sorted()
+//                .collect(Collectors.toList());
+//        System.out.println("In Alphabet: " + sortedAlphabet);
+//
+//        List<String> sortedLetter = words.stream()
+//                .sorted(Comparator.comparingInt(String::length))
+//                .collect(Collectors.toList());
+//        System.out.println("For Symbol: "+ sortedLetter);
+//
+//        List<String> sortedReverseAlphabet = words.stream()
+//                .sorted(Comparator.reverseOrder())
+//                .collect(Collectors.toList());
+//        System.out.println("In Reverse Alphabet: " + sortedReverseAlphabet);
+//
+//        List<String> sortedReverseLetter = words.stream()
+//                .sorted(Comparator.comparingInt(String::length).reversed())
+//                .collect(Collectors.toList());
+//        System.out.println("In Reverse Letter: " + sortedReverseLetter);
+//
+//        Map<Integer, List<String>> groupedByLength = words.stream()
+//                .collect(Collectors.groupingBy(String::length));
+//        System.out.println("Grouped by length: " + groupedByLength);
+//
+//        List<String> shortWord = words.stream()
+//                .filter(word -> word.length()<5)
+//                .collect(Collectors.toList());
+//        System.out.println("Word which < 5: "+ shortWord);
+
+        Random random = new Random();
+        String randomWord = words.stream()
+                .skip(random.nextInt(words.size()))
+                .findFirst()
+                .orElse(" ");
+        System.out.println("Your favorite word in the world is - " + randomWord);
+    }
+    public float has (){
+      float sgs = 126.6f;
+      return sgs;
+
+    }
+ @Test
+    public  static void namely (){
+        Sashaconstructor mali = new Sashaconstructor(37, (char) 7,23.7f,true);
+     System.out.println(mali.calcolytor+" : "+ mali.calcolytor2 +" / "+mali.calcolytor3+" $ "+ mali.calcolytor4);
+mali.setCalcolytor4(false);
+     System.out.println(mali.calcolytor4);
+    mali.setCalcolytor2((char)3);
+     System.out.println(mali.getCalcolytor2());
+    }
+
+@Test
+    public void Rosluna(){
+
+        for  (int k1 = 0; k1 < 1;k1++){
+        if (k1==0){
+            for (k1 = 0; k1 < 1; k1++){
+                System.out.println("             / ");
+        for (int k = 0; k < 1;k++){
+            if (k==0){
+                for (k = 0; k < 1; k++){
+                    System.out.println("            / ");
+
+                    for (int e = 0; e < 1;e++){
+                        if (e==0){
+                            for (e = 0; e < 1; e++){
+                                System.out.println("           / ");            }
+            }
+    }
+
+                    for (int t = 0; t < 1;t++){
+                        if (t==0){
+                            for (t = 0; t < 1; t++){
+                                System.out.println("          / ");}
+
+}}}
+
+
+                for (int l = 0; l < 1;l++){
+                    if (l==0){
+                        for (l = 0; l < 1; l++){
+                            System.out.println("         / ");
+            }
+        }}
 
 }
+            for (int u = 0; u < 1;u++){
+                if (u==0) {
+                    for (u = 0; u < 1; u++) {
+                        System.out.println("        /+ГЕЙ КЛАБ+ ");
+                    }
+                }}}}}}}
+@Test
+public void tree(){
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (i==j) {
+                System.out.print("*");
+            } else {
+
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+    }
+}
+}
+
+
---
+++ src/test/java/zhenia/Sashaconstructor.java
@@ -0,0 +1,53 @@
+package zhenia;
+
+public class Sashaconstructor {
+
+    int calcolytor;
+    char calcolytor2;
+    float calcolytor3;
+
+    public int getCalcolytor() {
+        return calcolytor;
+    }
+
+    public void setCalcolytor(int calcolytor) {
+        this.calcolytor = calcolytor;
+    }
+
+    public char getCalcolytor2() {
+        return calcolytor2;
+    }
+
+    public void setCalcolytor2(char calcolytor2) {
+        this.calcolytor2 = calcolytor2;
+    }
+
+    public float getCalcolytor3() {
+        return calcolytor3;
+    }
+
+    public char setCalcolytor3(float calcolytor3) {
+        this.calcolytor3 = calcolytor3;
+        return 8;
+    }
+
+    public boolean isCalcolytor4() {
+        return calcolytor4;
+    }
+
+    public void setCalcolytor4(boolean calcolytor4) {
+        this.calcolytor4 = calcolytor4;
+    }
+
+    boolean calcolytor4;
+
+    public Sashaconstructor(int calcolytor, char calcolytor2, float calcolytor3, boolean calcolytor4) {
+        this.calcolytor = calcolytor;
+        this.calcolytor2 = calcolytor2;
+        this.calcolytor3 = calcolytor3;
+        this.calcolytor4 = calcolytor4;
+    }
+
+
+
+}
