745a7f2fb229cf50bc0fe385aa94611219df218f
Alexkylymystyi <smeshkin3434@icloud.com>
2023-09-14 11:36:09 +0300

    zmina

--- src/test/java/oleksandr/webtesting/optimoveConect/pages/OptimoveNew.java
+++ src/test/java/oleksandr/webtesting/optimoveConect/pages/OptimoveNew.java
@@ -13,7 +13,6 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
 
-
 public class OptimoveNew extends BasePage {
     public OptimoveNew openOptimove() {
         DriverPoll.getDriver().get("https://www.optimove.com/");
@@ -24,47 +23,78 @@ public class OptimoveNew extends BasePage {
     private List<WebElement> company;
     @FindBy(xpath = "//div[@class='header-nav__item  header-nav__item--is-child  d-mobile  d-desktop ']/a[contains(text(),'Careers')]")
     private WebElement her23;
+    @FindBy(xpath = "//a[@class='cookiebanner__close d-block']")
+    private WebElement dblok;
+    @FindBy(xpath = "//div[@class='selectric']")
+    private WebElement selekctric;
+    @FindBy(xpath = "//ul/li[contains(text(),'UKR')]")
+    private WebElement UKR;
+    @FindBy(xpath = "//ul/li[contains(text(),'UKR')]")
+    private WebElement UKR2;
 
     public OptimoveNew howerOverCompany() {
-        WebElement companyElement = DriverPoll.getDriver().findElement(By.xpath("//*[text()='Company']"));
         Actions action = new Actions(DriverPoll.getDriver());
         action.moveToElement(company.get(3)).perform();
-        return this;
-    }
-
-    public OptimoveNew clikcareers() {
+        action.moveToElement(her23);
         click(her23);
+        sleep(3);
+        while (DriverPoll.getDriver().findElements(By.xpath("//iframe")).size() < 6) {
+            Actions actions = new Actions(DriverPoll.getDriver());
+            actions.moveByOffset(1, 1).perform();
+        }
+        sleep(3);
+        DriverPoll.getDriver().switchTo().frame(frame);
+        click(close);
+        sleep(2);
+        DriverPoll.getDriver().switchTo().defaultContent();
+
+        DriverPoll.getDriver().findElement(By.xpath("//a[@class='cookiebanner__close d-block']")).click();
+        DriverPoll.getDriver().findElements(By.xpath("//div[@class='selectric']")).get(0).click();
+        action.moveToElement(DriverPoll.getDriver().findElement(By.xpath("//ul/li[contains(text(),'UKR')]")));
+        sleep(2);
+        DriverPoll.getDriver().findElement(By.xpath("//ul/li[contains(text(),'UKR')]")).click();
         sleep(3);
         return this;
     }
 
 
-
-@FindBy(xpath = "//*[@id='hubspot-conversations-iframe']")
+    @FindBy(xpath = "//*[@id='hubspot-conversations-iframe']")
     private WebElement frame;
-@FindBy(xpath = "//div[@class='VizExIcon__IconWrapper-sc-1qu9za0-0 kximul']")
-private WebElement close;
-    public OptimoveNew iframedroch(){
+    @FindBy(xpath = "//div[@class='VizExIcon__IconWrapper-sc-1qu9za0-0 kximul']")
+    private WebElement close;
+
+    public OptimoveNew iframedroch() {
+
+        while (DriverPoll.getDriver().findElements(By.xpath("//iframe")).size() < 6) {
+            Actions actions = new Actions(DriverPoll.getDriver());
+            actions.moveByOffset(1, 1).perform();
+        }
         sleep(3);
         DriverPoll.getDriver().switchTo().frame(frame);
         click(close);
         DriverPoll.getDriver().switchTo().defaultContent();
+        DriverPoll.getDriver().findElement(By.xpath("//a[@class='cookiebanner__close d-block']")).click();
         return this;
     }
 
-    public OptimoveNew ckrol3(){
-        scroll(4200);
-       sleep(5);
-        return  this;
-
-}
-@FindBy(xpath = "//div[@class='selectric-wrapper']")
+    @FindBy(xpath = "//div[@class='select select--job-locations']")
     private WebElement all;
-    public OptimoveNew allin(){
-        click(all);
+    @FindBy(xpath = "//div[@class='header-nav__item  header-nav__item--is-child  d-mobile  d-desktop ']")
+    private WebElement carrer;
+    @FindBy(xpath = "//*[@class='site-logo entered lazyloaded']")
+    private WebElement move;
+
+    public OptimoveNew carrers() {
+        scroll(4400);
+        Actions actions = new Actions(DriverPoll.getDriver());
+        actions.moveToElement(move).perform();
+        sleep(5);
         return this;
     }
 }
+
+
+
 
 
 
