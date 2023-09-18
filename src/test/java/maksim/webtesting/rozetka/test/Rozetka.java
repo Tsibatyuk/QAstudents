b3d2f05939ce871dec672ea4c4164b642ccd48ef
Tsibuklo <fisher481516@gmail.com>
2023-09-18 11:38:46 +0300

    s

--- src/test/java/maksim/webtesting/rozetka/test/Rozetka.java
+++ src/test/java/maksim/webtesting/rozetka/test/Rozetka.java
@@ -42,6 +42,7 @@ public class Rozetka extends BaseTest {
         lst.openrozetka()
                 .webElement();
     }
+
     @Test
     public void screachHP(){
         Mainpage hp = new Mainpage();
